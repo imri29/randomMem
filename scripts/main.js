29f674f60d1ef66b50c851f055e5ac10a73506bf
Imri <imri294@gmail.com>
2018-04-01 22:53:11 +0300

    benya and Imri big change

---
+++ scripts/main.js
@@ -0,0 +1,55 @@
+const RANDOM_API_URL = "http://www.memeking.co.il/api/random-meme";
+const ERROR_MESSAGE =
+  "https://fthmb.tqn.com/qLv10Pgd30kCy7OxXacwOWKxZ8M=/768x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/shutterstock_325494917-5a68d8403418c600190a3e1f.jpg";
+const MOBILE_SIZE = 767;
+
+$(document).ready(function() {
+  const button = $("button");
+  const spinner = $(".spinner");
+  const memes = $(".memes");
+  const modal = $(".modal");
+  const iframe = $("iframe");
+
+  spinner.hide(memes, modal);
+
+  button.click(() => {
+    button.html('<div id="loading"></div>');
+    $.get(RANDOM_API_URL, loadAndInsertRandomMeme);
+  });
+
+  modalController.init(memes, modal);
+
+  /* Global Functions*/
+
+  function loadAndInsertRandomMeme(data, status) {
+    if (status === "success") {
+      const memeAddress = data.randomMeme.urlPath;
+      const memeId = data.randomMeme.id;
+      const category = data.randomMeme.category;
+      memes.attr("src", memeAddress);
+      button.html("<span>מם רנדומלי</span>");
+      iframe
+        .attr(
+          "src",
+          `http://www.memeking.co.il/memes/${category}/generator/${memeId}/normalFormat`
+        )
+        .css(getIframeSize());
+    } else {
+      memes.attr("src", ERROR_MESSAGE);
+    }
+  }
+});
+
+function getIframeSize() {
+  if (window.innerWidth > MOBILE_SIZE) {
+    return {
+      height: "95%",
+      width: "100%"
+    };
+  } else {
+    return {
+      height: "100%",
+      width: "100%"
+    };
+  }
+}
---
+++ scripts/modal.js
@@ -0,0 +1,21 @@
+const modalController = {
+
+   init: function(memes, modal) {
+      // Modal Behavior
+      memes.click(function() {
+         modal.slideDown();
+      });
+
+      // close it on X
+      $(".close").click(function() {
+         modal.slideUp();
+      });
+
+      //close it when clicking on dark area
+      $(window).click(function(e) {
+         if ($(e.target).is(modal)) {
+            modal.slideUp();
+         }
+      });
+   }
+};
